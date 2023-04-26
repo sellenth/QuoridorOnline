355219dbdf5e44c86e9515dc384449d98963f0ba
halston <sellenth@oregonstate.edu>
2023-04-25 22:27:52 -0700

    stylize header buttons more

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,7 @@ import { useSupabase } from './supabase-provider'
 function HeaderButton({ link, text, color, authed = true }: { link: string, text: string, color?: string, authed?: boolean }) {
   let url = authed ? link : '/signin'
     return (
-        <Link className={`font-display text-center hover:text-theme-100 ${color ? color : "text-gray-200"} text-xl font-bold py-4 px-12 hover:border-b-2 border-b-[1px] pb-[20px] hover:pb-[19px]`} as={link} href={url}>
+        <Link className={`btn-transition hover:bg-thm-500 font-display text-center hover:text-theme-100 ${color ? color : "text-gray-200"} text-xl font-bold py-4 px-12 hover:border-b-2 border-b-[1px] pb-[20px] hover:pb-[19px]`} as={link} href={url}>
             {text}
         </Link>
     )
