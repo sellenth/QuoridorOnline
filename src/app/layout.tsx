f7fd78cea2e3b85b0d3ebf0a5ef6c1f708d49362
halston <sellenth@oregonstate.edu>
2023-04-25 22:27:26 -0700

    mobile styling tweak

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default async function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className="flex flex-col h-screen">
+      <body className="flex flex-col">
         <SupabaseProvider session={session}>
           <SupabaseListener serverAccessToken={session?.access_token} />
           <Header />
