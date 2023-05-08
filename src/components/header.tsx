4bd9de2015a679c13c37f8b4de61c5268dd2624f
halston <sellenth@oregonstate.edu>
2023-05-07 18:11:52 -0700

    hide Home button on mobile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,10 +5,12 @@ import Link from 'next/link'
 import { useSupabase } from './supabase-provider'
 
 
-function HeaderButton({ link, text, color, authed = true }: { link: string, text: string, color?: string, authed?: boolean }) {
+function HeaderButton({ link, text, color, authed = true, className = '' }: { link: string, text: string, color?: string, authed?: boolean, className?: string }) {
   let url = authed ? link : '/signin'
     return (
-        <Link className={`btn-transition hover:bg-thm-500 font-display text-center hover:text-theme-100 ${color ? color : "text-gray-200"} text-xl font-bold py-4 px-1 text-xs md:text-base md:px-8 lg:px-12 hover:border-b-2 border-b-[1px] pb-[20px] hover:pb-[19px]`} as={link} href={url}>
+      <Link className={`${className} btn-transition hover:bg-thm-500 font-display text-center hover:text-theme-100
+                        ${color ? color : "text-gray-200"} text-xl font-bold py-4 px-1 text-xs md:text-base
+                        md:px-8 lg:px-12 hover:border-b-2 border-b-[1px] pb-[20px] hover:pb-[19px]`} as={link} href={url}>
             {text}
         </Link>
     )
@@ -22,7 +24,7 @@ export default function Header() {
 
   return (
     <div className=" z-10 w-full flex items-center justify-center justify-items-center grid-cols-5">
-      <HeaderButton link="/" text="HOME"/>
+      <HeaderButton className="hidden md:block" link="/" text="HOME"/>
       <HeaderButton link="/scoreboard" text="SCOREBOARD" />
       <HeaderButton link="/friendslist" text="FRIENDS" authed={isAuthed}/>
       <HeaderButton link="/game-invites" text="GAMES" authed={isAuthed}/>
