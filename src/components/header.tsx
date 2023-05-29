ed1a9184617f00e76c39d77fce30787e944642af
halston <sellenth@oregonstate.edu>
2023-05-29 09:20:52 -0700

    disable prefetch on some finicky routes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,10 +6,11 @@ import { useSupabase } from './supabase-provider'
 import { ToastContainer } from 'react-tiny-toast';
 
 
-function HeaderButton({ link, text, color, authed = true, className = '' }: { link: string, text: string, color?: string, authed?: boolean, className?: string }) {
+function HeaderButton({ link, text, color, authed = true, className = '', prefetch=false }: { link: string, text: string, color?: string, authed?: boolean, className?: string, prefetch?: boolean }) {
   let url = authed ? link : '/signin'
     return (
-      <Link className={`${className} btn-transition hover:bg-thm-500 font-display text-center hover:text-theme-100
+      <Link prefetch={prefetch}
+            className={`${className} btn-transition hover:bg-thm-500 font-display text-center hover:text-theme-100
                         ${color ? color : "text-gray-200"} text-sm md:text-base font-bold pt-4 px-1
                         md:px-8 lg:px-12 hover:border-b-2 border-b-[1px] pb-[5px] md:pb-[20px] hover:pb-[4px] md:hover:pb-[19px]`} as={link} href={url}>
             {text}
@@ -34,10 +35,10 @@ export default function Header() {
 
   return (
     <div className="z-10 w-full flex items-center justify-center justify-items-center grid-cols-5">
-      <HeaderButton className="hidden sm:block" link="/" text="HOME"/>
-      <HeaderButton link="/scoreboard" text="SCOREBOARD" />
-      <HeaderButton link="/friendslist" text="FRIENDS" authed={isAuthed}/>
-      <HeaderButton link="/game-invites" text="GAMES" authed={isAuthed}/>
+      <HeaderButton className="hidden sm:block" link="/" text="HOME" prefetch={true} />
+      <HeaderButton link="/scoreboard" text="SCOREBOARD" prefetch={true} />
+      <HeaderButton link="/friendslist" text="FRIENDS" authed={isAuthed} />
+      <HeaderButton link="/game-invites" text="GAMES" authed={isAuthed} />
       <HeaderButton link="/account" text={username ? "ACCOUNT" : "LOGIN"} color="text-theme-500" authed={isAuthed} />
       < ToastContainer />
     </div>
