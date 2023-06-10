c12640fd16eba34b35462e54aa92685677625bd6
halston <sellenth@oregonstate.edu>
2023-06-10 10:30:02 -0700

    fix a warning for prefetch=true deprecated

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,8 +8,11 @@ import { ToastContainer } from 'react-tiny-toast';
 
 function HeaderButton({ link, text, color, authed = true, className = '', prefetch=false }: { link: string, text: string, color?: string, authed?: boolean, className?: string, prefetch?: boolean }) {
   let url = authed ? link : '/signin'
+  let attr = {};
+  if (!prefetch) attr = { prefetch: false };
+
     return (
-      <Link prefetch={prefetch}
+      <Link { ...attr }
             className={`${className} btn-transition hover:bg-thm-500 font-display text-center hover:text-theme-100
                         ${color ? color : "text-gray-200"} text-sm md:text-base font-bold pt-4 px-1
                         md:px-8 lg:px-12 hover:border-b-2 border-b-[1px] pb-[5px] md:pb-[20px] hover:pb-[4px] md:hover:pb-[19px]`} as={link} href={url}>
