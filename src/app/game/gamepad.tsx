35dfe33925c05a3ed0601cf530eebe5f915af49a
halston <sellenth@oregonstate.edu>
2023-10-04 18:14:01 -0700

    change to one rotate button in pawn mode

--- src/app/game/gamepad.tsx
+++ src/app/game/gamepad.tsx
@@ -71,23 +71,25 @@ export const GamePad = ({engine}: Props) => {
                     placingFences ? 'PLACE' : 'MOVE'}
             </p>
         </button>
-        <button onClick={placingFences ? left : () => { if (!autoRotate) prev()}} className={`border border-gray-200 text-gray-200 rounded-md h-10 w-28 ${autoRotate ? 'invisible' : ''}`}>
-            <svg className="m-auto h-[90%]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 010-1.06l7.5-7.5a.75.75 0 111.06 1.06L9.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5z" clipRule="evenodd" />
-            </svg>
-        </button>
         {placingFences ?
-            <button onClick={rotate} className="border border-gray-200 text-gray-200 rounded-md h-10 w-28">
-                <svg className="m-auto h-[90%]" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" strokeWidth={0} stroke="currentColor">
-                    <path fillRule="evenodd" d="M4.755 10.059a7.5 7.5 0 0112.548-3.364l1.903 1.903h-3.183a.75.75 0 100 1.5h4.992a.75.75 0 00.75-.75V4.356a.75.75 0 00-1.5 0v3.18l-1.9-1.9A9 9 0 003.306 9.67a.75.75 0 101.45.388zm15.408 3.352a.75.75 0 00-.919.53 7.5 7.5 0 01-12.548 3.364l-1.902-1.903h3.183a.75.75 0 000-1.5H2.984a.75.75 0 00-.75.75v4.992a.75.75 0 001.5 0v-3.18l1.9 1.9a9 9 0 0015.059-4.035.75.75 0 00-.53-.918z" clipRule="evenodd" />
+            <button onClick={left} className={`border border-gray-200 text-gray-200 rounded-md h-10 w-28 ${autoRotate ? 'invisible' : ''}`}>
+                <svg className="m-auto h-[90%]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                    <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 010-1.06l7.5-7.5a.75.75 0 111.06 1.06L9.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5z" clipRule="evenodd" />
                 </svg>
             </button>
             : <div />}
-        <button onClick={placingFences ? right : () => { if (!autoRotate) next()}} className={`border border-gray-200 text-gray-200 rounded-md h-10 w-28 ${autoRotate ? 'invisible' : ''}`}>
-            <svg className="m-auto h-[90%]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z" clipRule="evenodd" />
+        <button onClick={placingFences ? rotate : () => { if (!autoRotate) next()}} className="border border-gray-200 text-gray-200 rounded-md h-10 w-28">
+            <svg className="m-auto h-[90%]" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" strokeWidth={0} stroke="currentColor">
+                <path fillRule="evenodd" d="M4.755 10.059a7.5 7.5 0 0112.548-3.364l1.903 1.903h-3.183a.75.75 0 100 1.5h4.992a.75.75 0 00.75-.75V4.356a.75.75 0 00-1.5 0v3.18l-1.9-1.9A9 9 0 003.306 9.67a.75.75 0 101.45.388zm15.408 3.352a.75.75 0 00-.919.53 7.5 7.5 0 01-12.548 3.364l-1.902-1.903h3.183a.75.75 0 000-1.5H2.984a.75.75 0 00-.75.75v4.992a.75.75 0 001.5 0v-3.18l1.9 1.9a9 9 0 0015.059-4.035.75.75 0 00-.53-.918z" clipRule="evenodd" />
             </svg>
         </button>
+        {placingFences ?
+            <button onClick={right} className={`border border-gray-200 text-gray-200 rounded-md h-10 w-28 ${autoRotate ? 'invisible' : ''}`}>
+                <svg className="m-auto h-[90%]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                    <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z" clipRule="evenodd" />
+                </svg>
+            </button> 
+            : <div />}
         <div>
             <Modal />
         </div>
